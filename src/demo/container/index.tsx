01373fe1a859ebc40ece77fff1bc75972c074346
ciyinhuang <ciyinhuang@gmail.com>
2018-11-04 17:46:44 +0800

    up

--- src/demo/container/index.tsx
+++ src/demo/container/index.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, SafeAreaView } from 'react-native'
 // import ActionSheetExample from '@popui/popui-rn/components/action-sheet/demo/basic';
 // import { ActionSheet2 } from "@popui/popui-rn"
-// import AllComp from '@popui/popui-rn/rn-kitchen-sink/allComponents'
-// import ResourceLoader from '../common/ResourceLoader'
-import { Button } from '@popui/popui-rn'
-// import { Button } from 'antd-mobile-rn';
+import AllComp from '@popui/popui-rn/rn-kitchen-sink/allComponents'
+import ResourceLoader from '../common/ResourceLoader'
+
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        {/* <SafeAreaView style={styles.container}> */}
-        {/* <AllComp /> */}
-        <Text>test</Text>
-        <Button>Button</Button>
-        {/* </SafeAreaView> */}
-      </View>
+      <ResourceLoader>
+        <SafeAreaView style={styles.container}>
+          <AllComp />
+        </SafeAreaView>
+      </ResourceLoader>
     )
   }
 }
