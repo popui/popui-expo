39dfd6f7c48a579671243bf93d57f8900aa2ff67
ciyinhuang <ciyinhuang@gmail.com>
2018-11-10 15:28:36 +0800

    0.0.8

--- src/screens/LinksScreen.tsx
+++ src/screens/LinksScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, StyleSheet } from 'react-native'
+import { ScrollView, StyleSheet, Text } from 'react-native'
 import { ExpoLinksView } from '@expo/samples'
 
 export default class LinksScreen extends React.Component {
@@ -12,6 +12,7 @@ export default class LinksScreen extends React.Component {
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
+        {/* <Text>Links:</Text> */}
         <ExpoLinksView />
       </ScrollView>
     )
